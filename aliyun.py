aab7fb22ad64c48ed6032ccf5ca8757ac6420677
future <136156629@qq.com>
2015-11-07 02:05:33 +0800

    Update aliyun.py

--- aliyun.py
+++ aliyun.py
@@ -6,6 +6,10 @@ author: Future@nku 20151106
 
 
 def first_puzzle():
+    print 'keayrod d  looks like keyd(b)oard'
+
+
+def second_puzzle():
     sentence1 = '''of zit kggd zitkt qkt ygxk ortfzoeqs wqlatzwqssl qfr zvg
  ortfzoeqs yggzwqssl.fgv oy ngx vqfz zg hxz zitd of gft soft.piv dgfn lgsxz
 ogfl qkt zitkt?zohl:hstqlt eiqfut zit ygkd gy zit fxdwtk ngx utz.zit hkgu
@@ -15,20 +19,15 @@ kqddtkl!'''
     result = sentence1
     for i in range(0, len(alphas)):
         result = result.replace(keybord[i], alphas[i])
-    print "first puzzle:\n", result.lower()
-
-
-def second_puzzle():
-    print "second puzzle:\n", 2*3*4*5*6/(4*3*2 * 2*1)
-    return 2*3*4*5*6/(4*3*2 * 2*1)
+    print "second puzzle:\n", result.lower()
 
 
 def third_puzzle():
-    result = bin(second_puzzle())[2:]
+    result = bin(2*3*4*5*6/(4*3*2 * 2*1))[2:]
     print "third puzzle:\n", result
 
 
 if __name__ == '__main__':
     first_puzzle()
-#    second_puzzle()
+    second_puzzle()
     third_puzzle()
